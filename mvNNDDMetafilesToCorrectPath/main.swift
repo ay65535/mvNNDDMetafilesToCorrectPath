2f3e66697983069b8e920c234321f766d43830c0
ay65535 <ay65535@icloud.com>
2015-03-14 23:30:26 +0900

    Initial Commit

---
+++ mvNNDDMetafilesToCorrectPath/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mvNNDDMetafilesToCorrectPath
+//
+//  Created by 安田充志 on 2015/03/14.
+//  Copyright (c) 2015年 ay65535. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
